c7fbed3d222a100296809d572a8f8737fec5f750
Bronson Chaker <50012659+BronsonChaker@users.noreply.github.com>
2026-07-23 21:07:11 +1000

    made changed for mobile users

--- src/components/Experience/ExpListItem.jsx
+++ src/components/Experience/ExpListItem.jsx
@@ -2,7 +2,7 @@ export default function ExpListItem({ image, name, role, date, grade }) {
   return (
     <div className="flex flex-row mt-5 w-full">
       {/* Logo */}
-      <div className="flex rounded-full justify-start items-center w-[10%]">
+      <div className="flex rounded-full justify-start items-center w-[10%] max-lg:w-[20%]">
         <img
           src={image}
           alt=""
@@ -13,13 +13,19 @@ export default function ExpListItem({ image, name, role, date, grade }) {
         {/* Name & Degree */}
         <div className="flex flex-col items-start justify-center">
           <p className="font-semibold text-sm">{name}</p>
-          <p className="font-normal text-sm text-gray-600">{role}</p>
-          <p className="font-normal text-sm text-gray-600">{grade}</p>
+          <p className="font-normal text-sm text-gray-600 max-lg:text-xs">
+            {role}
+          </p>
+          <p className="font-normal text-sm text-gray-600 max-lg:text-xs">
+            {grade}
+          </p>
         </div>
 
         {/* Timeline */}
-        <div className="flex justify-end">
-          <p className="text-sm text-gray-400">{date}</p>
+        <div className="flex justify-end max-lg:wrap">
+          <p className="text-sm text-gray-400 max-lg:text-xs max-lg:flex">
+            {date}
+          </p>
         </div>
       </div>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <div className="flex mt-5">
-      <div className="w-[70%]">
+    <div className="flex mt-5 max-lg:flex-col">
+      <div className="w-[70%] max-lg:w-11/12 ">
         <h1 className="text-5xl font-medium text-headings">Bronson Chaker</h1>
 
-        <p className="mt-4 text-lg text-gray-500 font-light">
+        <p className="mt-4 text-lg text-gray-500 font-light ">
           Recent graduate from Charles Sturt Univeristy looking for a Graduate
           or Intern Software Engineering position.
         </p>
       </div>
 
-      <div className="flex items-center">
+      <div className="flex items-center max-lg:hidden">
         <img
           src={display_picture}
           alt=""
